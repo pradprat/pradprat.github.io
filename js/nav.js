9f8d432d928fbdc4e5550e733f6c54c0c4c68af1
pradprat <pradikafp14@gmail.com>
2018-12-27 20:27:44 +0700

    complete

--- js/nav.js
+++ js/nav.js
@@ -1,24 +1,81 @@
-document.addEventListener("DOMContentLoaded",function() {
+document.addEventListener('DOMContentLoaded',function() 
+{
 
-    var elems = document.querySelectorAll(".sidenav");
+    var elems = document.querySelectorAll('.sidenav');
     M.Sidenav.init(elems);
     loadNav();
 
-    function loadNav() {
+    function loadNav() 
+    {
         var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {  
-            if (this.readyState == 4) {
-                if (this.status != 200) {
+        xhttp.onreadystatechange = function () 
+        {  
+            if (this.readyState == 4) 
+            {
+                if (this.status != 200) 
+                {
                     return;
                 }
-                document.querySelectorAll(".topnav",".sidenav").forEach(function (elm) {  
+
+                document.querySelectorAll('.topnav').forEach(function (elm) 
+                {  
                     elm.innerHTML = xhttp.responseText;
+                });
+
+                document.querySelectorAll('.sidenav').forEach(function (elm) 
+                {  
+                    elm.innerHTML = xhttp.responseText;
+                });
+
+                document.querySelectorAll('.sidenav a','.topnav a').forEach(function(elm)
+                {
+                    elm.addEventListener('click',function (event) 
+                    {
+                        var sidenav = document.querySelector('.sidenav');
+                        M.Sidenav.getInstance(sidenav).close();
+
+                        page = event.target.getAttribute('href').substr(1);
+                        loadPage(page);
+                    });
                 });
             }
         };
-        // xhttp.open("GET","nav.html",true);
+
+        xhttp.open('GET','../assets/html/nav.html',true);
         xhttp.send();
         
+    }
+
+    var page = window.location.hash.substr(1);
+    if (page == '')
+    {
+        page = 'home';
+    }
+    loadPage(page);
+
+    function loadPage(page) 
+    {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () 
+        {
+            if (this.readyState == 4)
+            {
+                var content = document.querySelector('#body-content');
+                if (this.status == 200)
+                {
+                    content.innerHTML = xhttp.responseText;
+                } else 
+                if (this.status == 404)
+                {
+                    content.innerHTML = '<p>Halaman tidak ditemukan.</p>';
+                } else
+                {
+                    content.innerHTML = '<p>Ups.. halaman tidak dapat diakses.</p>'
+                }
+            }
+        };
+        xhttp.open('GET','../assets/html/pages/' + page + '.html',true);
+        xhttp.send();
     }
     
 });
